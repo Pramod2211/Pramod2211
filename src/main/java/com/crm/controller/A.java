fcd0223cc779a386fec81629ca66692a5ca11e22
pramod <pramodchalavadi2002@gmail.com>
2024-12-03 14:59:26 +0530

    test 1

---
+++ src/main/java/com/crm/controller/A.java
@@ -0,0 +1,10 @@
+package com.crm.controller;
+
+public class A {
+    private int y = 10;
+
+    public static void main(String[] args) {
+        A a1 = new A();
+        System.out.println(a1.y);
+    }
+}
---
+++ src/main/java/com/crm/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.crm.repository;
+
+import com.crm.entity.Comment;
+import com.crm.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    interface CommentRepository extends JpaRepository<Comment, Long> {
+    }
+}
